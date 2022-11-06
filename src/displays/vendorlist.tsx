37a3cbdbd5fc0ac9570edc9e2fe982207c1a55ed
Daniel Brook <danb358@gmail.com>
2022-11-06 13:23:23 -0500

    Add filtering capability to the vendor list tab

--- src/displays/vendorlist.tsx
+++ src/displays/vendorlist.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 
+import { Input } from 'semantic-ui-react';
+
 import { inject, observer } from 'mobx-react';
 
 import VendorListItem from '../widgets/vendorlistitem';
 
-interface TaskListProps {
+interface VendorListProps {
   alphaSort: boolean;
 //   showStore?: TradeShowData;
   showStore?: any;  // Workaround for now ... FIXME: How to use a type?
 }
 
+interface VendorListState {
+  filterText: string;
+}
+
 @inject('showStore') @observer
-export default class TaskList extends React.Component<TaskListProps> {
+export default class VendorList extends React.Component<VendorListProps, VendorListState> {
+  constructor(props: any, state: any) {
+    super(props, state);
+    this.state = { filterText: '' };
+  }
+
   render() {
     const { alphaSort, showStore: { boothVendors, vendorsWithActions } } = this.props;
+    const { filterText } = this.state;
+    const lowerFilterText = filterText.toLowerCase();
 
     // FIXME: Maybe make this whole thing sortable by vendor -OR- booth number?
     const tempVendorStat = Array.from(boothVendors, ([key, value]) => {
       return {boothId: key, boothNum: value.boothNum, vendor: value.vendor};
+    }).filter((item: any) => {
+      if (lowerFilterText !== '') {
+        return item.vendor.toLowerCase().includes(lowerFilterText);
+      }
+      return true;
     }).sort((a: any, b: any) => {
       if (alphaSort) {
         return a.vendor < b.vendor ? -1 : (a.vendor > b.vendor ? 1 : 0);
@@ -37,9 +55,24 @@ export default class TaskList extends React.Component<TaskListProps> {
     });
 
     return (
-      <div className='tabInnerLayout BCIouterVendorList'>
-        {vendorRows}
-      </div>
+      <>
+        <Input fluid
+               placeholder='Filter Vendors'
+               action={{ icon: 'x', onClick: this.clearFilterText }}
+               value={filterText}
+               onChange={this.updateFilterText} />
+        <div className='tabInnerLayout BCIouterVendorList'>
+          {vendorRows}
+        </div>
+      </>
     );
   }
+
+  private updateFilterText = (e: any, data: any) => {
+    this.setState({ filterText: e.target.value });
+  };
+
+  private clearFilterText = (e: any, data: any) => {
+    this.setState({ filterText: '' });
+  };
 }
