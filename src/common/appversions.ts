8c20f280099fb00a439d0889bf7936f3bb5a5764
Daniel Brook <danb358@gmail.com>
2022-11-19 10:11:11 -0500

    Put version string constants in their own common file

---
+++ src/common/appversions.ts
@@ -0,0 +1,10 @@
+/*
+ * Application-level version descriptors for About Window
+ */
+
+export const applicationVersion: string = '1.0.0';
+export const copyrightYearRange: string = '2022';
+export const uiLibrary: string = 'React 18.0.0';
+export const uiControls: string = 'Semantic UI React 2.1.3';
+export const stateManagement: string = 'MobX 6.6.2, MobX-React 7.5.3';
+export const database: string = 'Dexie 3.2.2';
--- src/widgets/aboutpanel.tsx
+++ src/widgets/aboutpanel.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 
 import { Header } from 'semantic-ui-react';
 
-export default class AboutPanel extends React.Component {
-  private uiLibrary: string = 'React 18.0.0';
-  private uiControls: string = 'Semantic UI React 2.1.3';
-  private stateManagement: string = 'MobX 6.6.2, MobX-React 7.5.3';
-  private database: string = 'Dexie 3.2.2';
+import {
+  applicationVersion,
+  copyrightYearRange,
+  uiLibrary,
+  uiControls,
+  stateManagement,
+  database,
+} from '../common/appversions';
 
+export default class AboutPanel extends React.Component {
   render() {
     return (
       <>
         <Header color='violet' as='h1'>BCI Trade Show Tracker</Header>
+        <p><b>Version {applicationVersion}</b></p>
         <p>A simple vendor interaction tracking application for Dealers attending BCI Trade Shows.</p>
-        <p>Copyright © 2022, Daniel Brook</p>
+        <p>Copyright © {copyrightYearRange}, Daniel Brook</p>
         <p>Not affiliated with Bradley Caldwell, Inc.</p>
         <Header color='orange' as='h3'>Application Details</Header>
         <p>Created with Create-React-App (CRA)</p>
-        <p>Core User Interface: {this.uiLibrary}</p>
-        <p>User Interface Controls: {this.uiControls}</p>
-        <p>State Management: {this.stateManagement}</p>
-        <p>IndexedDB Interface: {this.database}</p>
+        <p>Core User Interface: {uiLibrary}</p>
+        <p>User Interface Controls: {uiControls}</p>
+        <p>State Management: {stateManagement}</p>
+        <p>IndexedDB Interface: {database}</p>
       </>
     );
   }
