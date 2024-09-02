75d576272257b78a93a6128e169941a758d786b6
Daniel Brook <danb358@gmail.com>
2024-09-02 12:49:14 -0400

    Version 2.1: adds capability to bulk-add PC and PB items for less clicking when initially setting up a show

--- src/common/appversions.ts
+++ src/common/appversions.ts
@@ -2,8 +2,8 @@
  * Application-level version descriptors for the About Window
  */
 
-export const applicationVersion: string = '2.0.0';
-export const copyrightYearRange: string = '2022-2023';
+export const applicationVersion: string = '2.1.0';
+export const copyrightYearRange: string = '2022-2024';
 export const uiLibrary: string = 'React 18.0.0';
 export const uiControls: string = 'Semantic UI React 2.1.3';
 export const stateManagement: string = 'MobX 6.6.2, MobX-React 7.5.3';
